32495a3c537475848d31dd4e1d1ca9e151ff4d92
ezekielharvey <zekeharvey@outlook.com>
2023-09-30 19:48:18 -0400

    Navbar finished

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">{`Hello, I'm${" "}`}</span>
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#12b3eb] to-[#6bdfdb]">{`Hello, I'm${" "}`}</span>
                       <br></br>
                       <TypeAnimation
                         sequence={[
@@ -20,7 +20,7 @@ const HeroSection = () => {
                           1000,
                           'a Mobile Developer',
                           1000,
-                          'a Muscisian',
+                          'a UI/UX Designer',
                           1000,
                         ]}
                         wrapper="span"
@@ -31,10 +31,10 @@ const HeroSection = () => {
                 </h1>
                 <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>I am a web developer. This is a random description</p>
                 <div>
-                  <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
+                  <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-200 text-white font-semibold'>
                     Hire Me
                   </button>
-                  <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 mt-3'>
+                  <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-800 mt-3'>
                     <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
                   </button>
                 </div>
