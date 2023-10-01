d2d393b086101dfe9b1fae216c007325b330b4bb
ezekielharvey <zekeharvey@outlook.com>
2023-10-01 14:27:58 -0400

    Project Section completed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import AboutSection from './components/AboutSection'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
+import ProjectSection from './components/ProjectSection'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mx-auto mt-24 px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectSection />
       </div>
     </main>
   )
