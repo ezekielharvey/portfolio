75034dbac73cf7889b6b271cf9647c6f12699d46
ezekielharvey <zekeharvey@outlook.com>
2023-10-02 02:09:45 -0400

    Project finished

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,51 +3,69 @@
 import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 const HeroSection = () => {
   return (
-    <section className='grid grid-cols-1 sm:grid-cols-12 my-4'>
-        <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                  <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-[#12b3eb] to-[#6bdfdb]">{`Hello, I'm${" "}`}</span>{" "}
-                      <br></br>
-                      <TypeAnimation
-                        sequence={[
-                          'Ezekiel Harvey',
-                          1000,
-                          'Web Developer',
-                          1000,
-                          'Mobile Developer',
-                          1000,
-                          'UI/UX Designer',
-                          1000,
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={Infinity}
-                      />
-                </h1>
-                <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>I am a web developer. This is a random description</p>
-                <div>
-                  <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-200 text-white font-semibold'>
-                    Hire Me
-                  </button>
-                  <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-800 mt-3'>
-                    <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
-                  </button>
+    <section className='lg:py-16'>
+        <div className='grid grid-cols-1 sm:grid-cols-12 my-4'>
+          <motion.div 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }} 
+            className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          >
+                  <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                    <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-[#12b3eb] to-[#6bdfdb]">{`Hello, I'm${" "}`}</span>{" "}
+                        <br></br>
+                        <TypeAnimation
+                          sequence={[
+                            'Ezekiel Harvey',
+                            1000,
+                            'Web Developer',
+                            1000,
+                            'Mobile Developer',
+                            1000,
+                            'UI/UX Designer',
+                            1000,
+                          ]}
+                          wrapper="span"
+                          speed={50}
+                          repeat={Infinity}
+                        />
+                  </h1>
+                  <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>I am a web developer. This is a random description</p>
+                  <div>
+                    <Link href="/contact">
+                      <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-200 text-white font-semibold'>
+                        Hire Me
+                      </button>
+                    </Link>
+                    <Link href="/">
+                      <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#2278FB] via-[#48AEEA] to-[#6BDFDB] hover:bg-slate-800 mt-3'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                      </button>
+                    </Link>
+                  </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+                className="col-span-4 place-self-center mt-4 lg:mt-0"
+              >
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                  <Image
+                    src='/images/ProfileImage.png'
+                    alt='Profile Image'
+                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                    width={300}
+                    height={300}
+                  />
                 </div>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-              <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                <Image
-                  src='/images/ProfileImage.png'
-                  alt='Profile Image'
-                  className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                  width={300}
-                  height={300}
-                />
-              </div>
-            </div>
+              </motion.div>
+        </div>
     </section>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import AboutSection from './components/AboutSection'
+import AchievementsSection from './components/AchievementsSection'
 import EmailSection from './components/EmailSection'
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
@@ -11,6 +12,7 @@ export default function Home() {
       <NavBar />
       <div className="container mx-auto mt-24 px-12 py-4">
         <HeroSection />
+        <AchievementsSection />
         <AboutSection />
         <ProjectSection />
         <EmailSection />
